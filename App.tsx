79f61dc48becd7e25a48a379c5d8c7611575c5f3
peanut996 <849421294godw@gmail.com>
2021-11-25 00:46:33 +0800

    Lint: 格式化代码

--- App.tsx
+++ App.tsx
@@ -7,19 +7,19 @@ import useColorScheme from './hooks/useColorScheme'
 import Navigation from './navigation'
 
 const App = () => {
-  const isLoadingComplete = useCachedResources()
-  const colorScheme = useColorScheme()
+    const isLoadingComplete = useCachedResources()
+    const colorScheme = useColorScheme()
 
-  if (!isLoadingComplete) {
-    return null
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    )
-  }
+    if (!isLoadingComplete) {
+        return null
+    } else {
+        return (
+            <SafeAreaProvider>
+                <Navigation colorScheme={colorScheme} />
+                <StatusBar />
+            </SafeAreaProvider>
+        )
+    }
 }
 
 export default App
