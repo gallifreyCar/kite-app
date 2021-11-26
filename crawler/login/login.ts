b8ee700e38f91bd3d70b550dd8e109a10d9d58ac
peanut996 <849421294godw@gmail.com>
2021-11-26 12:43:38 +0800

    Update: http-client改为node-fetch

--- crawler/login/login.ts
+++ crawler/login/login.ts
@@ -1,19 +1,46 @@
 import axios from 'axios'
 import * as Buffer from 'buffer'
-import FormData from 'form-data'
 import * as CryptoJS from 'crypto-js'
 import * as qs from 'qs'
+import * as cookie from 'cookie'
+import fetch from 'node-fetch'
 
+interface CreateLogin {
+  username: string
+  password: string
+  captcha?: string
+  captcha_key?: string
+}
+
+
+
+const HEADERS = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language": "ja,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+    "Referer": "https://authserver.sit.edu.cn/authserver/login"
+}
 const LOGIN_URL = 'https://authserver.sit.edu.cn/authserver/login'
 const NEED_CAPTCHA_URL = 'https://authserver.sit.edu.cn/authserver/needCaptcha.html'
 const CAPTCHA_URL = 'https://authserver.sit.edu.cn/authserver/captcha.html'
-const DDDDOCR_URL = 'http://192.168.2.27:22334/recognition'
+const DDDDOCR_URL = 'http://127.0.0.1:5000/recognition'
+
+const needCaptchaCheck = async (cookie: string,account: string) => {
+    const res = await axios.get(`${NEED_CAPTCHA_URL}?username=${account}&pwdEncrypt2=pwdEncryptSalt`,{
+        headers: {
+            "Cookie": cookie,
+            "Referer": "https://authserver.sit.edu.cn/authserver/login"
+        }
+    })
+    return res.data
+}
 
 export const login = async (username: string, password: string) => {
     const res = await axios.get(LOGIN_URL)
-    const cookie = res.headers['set-cookie']![0]
+    const originCookie = res.headers['set-cookie']![0]
+    const cookie = filterCookie(originCookie)
     const loginHtml = res.data
-    const needCaptcha = loginHtml.includes('验证码')
+    const needCaptcha = await needCaptchaCheck(cookie, username)
     const pwdSalt = getPwdSalt(loginHtml)
     const captchaImage = await getCaptchaImageBase64(CAPTCHA_URL, cookie)
     const captchaResult = await recognizeCaptcha(captchaImage)
@@ -24,7 +51,7 @@ export const login = async (username: string, password: string) => {
         password,
         captchaResult,
         pwdSalt,
-        cookie
+        originCookie
     )
     return 'Hello World'
 }
@@ -66,7 +93,7 @@ const loginWithAuth = async (
 ) => {
     const params = {
         username,
-        password: encryptPassword(password, salt),
+        password: encryptAES(password, salt),
         captchaResponse: captcha,
         lt: getLT(html),
         dllt: 'userNamePasswordLogin',
@@ -74,23 +101,25 @@ const loginWithAuth = async (
         _eventId: 'submit',
         rmShown: '1',
     }
-    const res = await axios.post(LOGIN_URL, qs.stringify(params), {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Cookie: cookie,
+    const res = await fetch("https://authserver.sit.edu.cn/authserver/login", {
+        "headers": {
+            ...HEADERS,
+            "content-type": "application/x-www-form-urlencoded",
+            cookie,
         },
-    })
+        "referrer": "https://authserver.sit.edu.cn/authserver/login",
+        "referrerPolicy": "no-referrer-when-downgrade",
+        body: qs.stringify(params),
+        "method": "POST",
+        // "mode": "cors",
+        'redirect': 'manual', // set to `manual` to extract redirect headers, `error` to reject redirect
+    });
     if (res.status === 301 || res.status === 302) {
-        return res.headers['set-cookie']
-    }
-    if (res.data.includes('您提供的用户名或者密码有误')) {
-        throw new Error('用户名或密码错误')
-    }
-    if (res.data.includes('无效的验证码')) {
-        throw new Error('验证码错误')
+        return res.headers
     }
     throw new Error('Login Failed')
 }
+
 
 const getLT = (html: string): string => {
     const lt = html.match(/<input type="hidden" name="lt" value="(.*?)"/)![1]
@@ -101,30 +130,38 @@ export const encryptPassword = (password: string, salt: string) => {
     return encryptAES(password, salt)
 }
 
-function getEncrypt(data: any, key0: any, iv0: any) {
-    key0 = key0.replace(/(^\s+)|(\s+$)/g, '')
-    var key = CryptoJS.enc.Utf8.parse(key0)
-    var iv = CryptoJS.enc.Utf8.parse(iv0)
+
+const $_chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+const _chars_len = $_chars.length;
+function _rds(len: number) {
+    var retStr = '';
+    let i;
+    for (i = 0; i < len; i++) {
+        retStr += $_chars.charAt(Math.floor(Math.random() * _chars_len));
+    }
+    return retStr;
+}
+const _gas = (data: string | CryptoJS.lib.WordArray, key0: string, iv0: string) => {
+    key0 = key0.replace(/(^\s+)|(\s+$)/g, "");
+    var key = CryptoJS.enc.Utf8.parse(key0);
+    var iv = CryptoJS.enc.Utf8.parse(iv0);
     var encrypted = CryptoJS.AES.encrypt(data, key, {
         iv: iv,
         mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7,
-    })
-    return encrypted.toString()
+        padding: CryptoJS.pad.Pkcs7
+    });
+    return encrypted.toString();
 }
-function encryptAES(data: any, salt: any) {
-    if (!salt) {
-        return data
+const encryptAES = (data: string, _p1: string) => {
+    if (!_p1) {
+        return data;
     }
-    var encrypted = getEncrypt(randomByte(64) + data, salt, randomByte(16))
-    return encrypted
+    var encrypted = _gas(_rds(64) + data, _p1, _rds(16));
+    return encrypted;
 }
-var allChars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'
-var charsLength = allChars.length
-function randomByte(len: any) {
-    var retStr = ''
-    for (let i = 0; i < len; i++) {
-        retStr += allChars.charAt(Math.floor(Math.random() * charsLength))
-    }
-    return retStr
+
+const filterCookie = (origin: string): string => {
+    const parseCookie = cookie.parse(origin)
+    delete parseCookie["path"]
+    return Object.keys(parseCookie).map(key => `${key}=${parseCookie[key]}`).join('; ')
 }
