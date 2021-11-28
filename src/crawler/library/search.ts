195d640fdb9ee777aec8b66b20a98b81cbec5d92
zzq <2428698039@qq.com>
2021-11-28 15:37:40 +0800

    Update: 更新图书馆模块结构

--- src/crawler/library/search.ts
+++ src/crawler/library/search.ts
@@ -1,19 +1,76 @@
-const search = async (request: SearchLibraryRequest):Promise<SearchLibraryResult> =>  {
+export enum SearchWay {
+    // 按任意词查询
+    Any = '',
+    // 标题名
+    Title = 'title',
+    // 正题名：一本书的主要名称
+    TitleProper = 'title200a',
+    // ISBN号
+    Isbn = 'isbn',
+    // 著者
+    Author = 'author',
+    // 主题词
+    SubjectWord = 'subject',
+    // 分类号
+    ClassNo = 'class',
+    // 控制号
+    CtrlNo = 'ctrlno',
+    // 订购号
+    OrderNo = 'orderno',
+    // 出版社
+    Publisher = 'publisher',
+    // 索书号
+    CallNo = 'callno',
+}
+
+export enum SortWay {
+    // 匹配度
+    MatchScore = 'score',
+    // 出版日期
+    PublishDate = 'pubdate_sort',
+    // 主题词
+    Subject = 'subject_sort',
+    // 标题名
+    Title = 'title_sort',
+    // 作者
+    Author = 'author_sort',
+    // 索书号
+    CallNo = 'callno_sort',
+    // 标题名拼音
+    Pinyin = 'pinyin_sort',
+    // 借阅次数
+    LoanCount = 'loannum_sort',
+    // 续借次数
+    RenewCount = 'renew_sort',
+    // 题名权重
+    TitleWeight = 'title200Weight',
+    // 正题名权重
+    TitleProperWeight = 'title200aWeight',
+    // 卷册号
+    Volume = 'title200h',
+}
+
+export enum SortOrder {
+    Asc = 'asc',
+    Desc = 'desc',
+}
+
+export const search = async (request: SearchLibraryRequest): Promise<SearchLibraryResult> => {
     let queryParams = {
-        'q': request.keyword,
-        'searchType': 'standard',
-        'isFacet': 'true',
-        'view': 'standard',
-        'searchWay': request.searchWay,
-        'rows': request.rows.toString(),
-        'sortWay': request.sortWay,
-        'sortOrder': request.sortOrder,
-        'hasholding': '1',
-        'searchWay0': 'marc',
-        'logical0': 'AND',
-        'page': request.page.toString()
+        q: request.keyword,
+        searchType: 'standard',
+        isFacet: 'true',
+        view: 'standard',
+        searchWay: request.searchWay,
+        rows: request.rows.toString(),
+        sortWay: request.sortWay,
+        sortOrder: request.sortOrder,
+        hasholding: '1',
+        searchWay0: 'marc',
+        logical0: 'AND',
+        page: request.page.toString(),
     }
 
     // TODO 数据爬取
-    return { bookList: [], currentPage: 0, resultCount: 0, totalPages: 0, useTime: 0 };
+    return { bookList: [], currentPage: 0, resultCount: 0, totalPages: 0, useTime: 0 }
 }
--- src/crawler/library/type.d.ts
+++ src/crawler/library/type.d.ts
@@ -1,102 +1,44 @@
-enum SearchWay {
-    // 按任意词查询
-    Any = '',
-    // 标题名
-    Title = 'title',
-    // 正题名：一本书的主要名称
-    TitleProper = 'title200a',
-    // ISBN号
-    Isbn = 'isbn',
-    // 著者
-    Author = 'author',
-    // 主题词
-    SubjectWord = 'subject',
-    // 分类号
-    ClassNo = 'class',
-    // 控制号
-    CtrlNo = 'ctrlno',
-    // 订购号
-    OrderNo = 'orderno',
-    // 出版社
-    Publisher = 'publisher',
-    // 索书号
-    CallNo = 'callno',
-}
-
-enum SortWay {
-    // 匹配度
-    MatchScore = 'score',
-    // 出版日期
-    PublishDate = 'pubdate_sort',
-    // 主题词
-    Subject = 'subject_sort',
-    // 标题名
-    Title = 'title_sort',
-    // 作者
-    Author = 'author_sort',
-    // 索书号
-    CallNo = 'callno_sort',
-    // 标题名拼音
-    Pinyin = 'pinyin_sort',
-    // 借阅次数
-    LoanCount = 'loannum_sort',
-    // 续借次数
-    RenewCount = 'renew_sort',
-    // 题名权重
-    TitleWeight = 'title200Weight',
-    // 正题名权重
-    TitleProperWeight = 'title200aWeight',
-    // 卷册号
-    Volume = 'title200h'
-}
-
-enum SortOrder {
-    Asc = 'asc',
-    Desc = 'desc',
-}
-
-
 interface SearchLibraryRequest {
     // 搜索关键字
-    keyword: string,
+    keyword: string
     // 搜索结果数量
-    rows: number,
+    rows: number
     // 搜索分页号
-    page: number,
+    page: number
     // 搜索方式
-    searchWay: SearchWay,
+    searchWay: SearchWay
     // 搜索结果的排序方式
-    sortWay: SortWay,
+    sortWay: SortWay
     // 搜索结果的升降序方式
-    sortOrder: SortOrder,
+    sortOrder: SortOrder
 }
 
 interface Book {
     // 图书号
-    bookId: string,
+    bookId: string
     // ISBN号
-    isbn: string,
+    isbn: string
     // 图书标题
-    title: string,
+    title: string
     // 图书作者
-    author: string,
+    author: string
     // 出版社
-    publisher: string,
+    publisher: string
     // 出版日期
-    publishDate: string,
+    publishDate: string
     // 索书号
-    callNo: string,
+    callNo: string
 }
 
 interface SearchLibraryResult {
     // 检索总结果数(所有页面的结果总数)
-    resultCount: number,
+    resultCount: number
     // 检索用时
-    useTime: number,
+    useTime: number
     // 当前页号
-    currentPage: number,
+    currentPage: number
     // 总页数
-    totalPages: number,
+    totalPages: number
     // 当前页面图书列表
-    bookList: Book[],
+    bookList: Book[]
 }
