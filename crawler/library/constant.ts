d3fa731d1ccea843af59e043ba67610d84a48386
zzq <2428698039@qq.com>
2021-11-28 11:07:51 +0800

    Add: 添加图书馆url定义

---
+++ crawler/library/constant.ts
@@ -0,0 +1,12 @@
+export const HEADERS = {
+    accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'ja,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7,zh;q=0.6',
+    'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+    Referer: 'https://authserver.sit.edu.cn/authserver/login'
+}
+
+export const HOME_URL = 'http://210.35.66.106'
+export const OPAC_URL = HOME_URL + '/opac'
+export const SEARCH_URL = OPAC_URL + '/search'
+export const BOOK_URL = OPAC_URL + '/book'
