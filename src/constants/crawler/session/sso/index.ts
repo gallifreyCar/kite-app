cba77d0e6561c882ccded1ed9b3b9b842d12e2aa
peanut996 <peanut996@qq.com>
2021-11-28 17:49:57 +0800

    Update: 修改路径以符合Windows

--- src/constants/crawler/session/sso/index.ts
+++ src/constants/crawler/session/sso/index.ts
@@ -2,5 +2,5 @@ export const SSO_LOGIN_URL = 'https://authserver.sit.edu.cn/authserver/login'
 export const NEED_CAPTCHA_URL = 'https://authserver.sit.edu.cn/authserver/needCaptcha.html'
 export const CAPTCHA_URL = 'https://authserver.sit.edu.cn/authserver/captcha.html'
 export const DDDDOCR_URL = 'http://127.0.0.1:5000/recognition'
-export const TESS_ENG_DATA_PATH = '.src/assets/tessdata'
+export const TESS_ENG_DATA_PATH = 'src/assets/tessdata'
 export const MAX_CAPTCHA_COUNT = 10
