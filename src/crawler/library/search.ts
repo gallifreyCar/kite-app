be37ba87aa8b5417ae7a92c4b713df946802d727
zzq <2428698039@qq.com>
2021-12-03 21:04:12 +0800

    Add: 图书馆搜索解析

--- src/crawler/library/search.ts
+++ src/crawler/library/search.ts
@@ -1,3 +1,9 @@
+import axios from 'axios'
+import $ from 'cheerio'
+
+import { SEARCH_URL } from '@/crawler/library/constant'
+import { parseInt } from 'lodash'
+
 export enum SearchWay {
     // 按任意词查询
     Any = '',
@@ -55,8 +61,15 @@ export enum SortOrder {
     Desc = 'desc',
 }
 
+/**
+ * 找到src字符串中第一个逗号分割的整数，并解析成number
+ * @param src
+ */
+const parseIntWithComma = (src: string): number =>
+    parseInt(/(\d+,?)+/.exec(src)![0].split(',').join(''))
+
 export const search = async (request: SearchLibraryRequest): Promise<SearchLibraryResult> => {
-    let queryParams = {
+    const queryParams = {
         q: request.keyword,
         searchType: 'standard',
         isFacet: 'true',
@@ -65,12 +78,52 @@ export const search = async (request: SearchLibraryRequest): Promise<SearchLibra
         rows: request.rows.toString(),
         sortWay: request.sortWay,
         sortOrder: request.sortOrder,
+        // 变量名为保持与图书馆系统参数一致, 禁止修改并忽略 IDE 警告
         hasholding: '1',
         searchWay0: 'marc',
         logical0: 'AND',
         page: request.page.toString(),
     }
 
-    // TODO 数据爬取
-    return { bookList: [], currentPage: 0, resultCount: 0, totalPages: 0, useTime: 0 }
+    const response = await axios.get(SEARCH_URL, {
+        params: queryParams,
+    })
+
+    const document = $(response.data)
+
+    const bookList = $('.resultTable > tbody:nth-child(1) > tr', document)
+        .map((index, element): Book => {
+            const bookCoverImage = $('.bookcover_img', element)
+            const getBookInfo = (selector: string) => $(selector, element).html()?.trim()!
+            return {
+                author: getBookInfo('.author-link'),
+                bookId: bookCoverImage.attr('bookrecno')!,
+                isbn: bookCoverImage.attr('isbn')!,
+
+                callNo: getBookInfo('.callnosSpan'),
+                publishDate: getBookInfo(
+                    'td:nth-child(4) > div:nth-child(1) > div:nth-child(3)'
+                ).split('出版日期:')[1],
+                publisher: getBookInfo('.publisher-link'),
+                title: getBookInfo('.title-link'),
+            }
+        })
+        .get()
+
+    const currentPage = parseInt($('div.meneame:nth-child(4) > b:nth-child(4)', document).text())
+    const resultNumAndTime = $('#search_meta > div:nth-child(1)', document).text()
+    const totalPages = parseIntWithComma(
+        $('div.meneame:nth-child(4) > span:nth-child(1)', document).text()
+    )
+    const resultCount = parseIntWithComma(resultNumAndTime)
+
+    const useTime = parseFloat(/检索时间: (\d+(?:\.\d+)?)/.exec(resultNumAndTime)![1])
+
+    return {
+        bookList,
+        currentPage,
+        totalPages,
+        resultCount,
+        useTime,
+    }
 }
