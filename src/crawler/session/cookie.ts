e08ac405db0e3bfb5e5bffd5f5993e35a6439f37
peanut996 <849421294godw@gmail.com>
2021-12-03 22:40:52 +0800

    Add: rename index.ts to cookie.ts

---
+++ src/crawler/session/cookie.ts
@@ -0,0 +1,13 @@
+import { initTesseractWorker, ssoLogin } from '@/crawler/session/sso/sso'
+import { eduLogin } from '@/crawler/session/edu/edu'
+
+export const LoginAndGetCookie = async (username: string, password: string) => {
+    await initTesseractWorker()
+    try {
+        const ssoCookie = await ssoLogin(username, password)
+        const eduCookie = await eduLogin(ssoCookie)
+        return eduCookie
+    } catch (e: any) {
+        throw new Error('获取Cookie失败, 原因: ' + e.message)
+    }
+}
