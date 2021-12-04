fc96f47559a26b9ee8ee1f4b4a89a93449a36ee0
zzq <2428698039@qq.com>
2021-12-04 12:01:12 +0800

    Refactor: 提取上网认证的常量定义

---
+++ src/constants/crawler/network/index.ts
@@ -0,0 +1,9 @@
+export const NETWORK_AUTH_INDEX_URL = 'http://172.16.8.70'
+export const NETWORK_AUTH_DRCOM_URL = NETWORK_AUTH_INDEX_URL + '/drcom'
+export const NETWORK_AUTH_LOGIN_URL = NETWORK_AUTH_DRCOM_URL + '/login'
+export const NETWORK_AUTH_CHECK_STATUS_URL = NETWORK_AUTH_DRCOM_URL + '/chkstatus'
+export const NETWORK_AUTH_LOGOUT_URL = NETWORK_AUTH_DRCOM_URL + '/logout'
+
+export const HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0',
+}
