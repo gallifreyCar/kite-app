2c0634fda178fb8b4bf0d659309806a68a93a767
RainSlide <RainSlide@noreply.codeberg.org>
2021-12-15 18:48:10 +0800

    将 kite 重命名为 Kite：iOS

--- ios/kiteTests/kiteTests.m
+++ ios/kiteTests/kiteTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface kiteTests : XCTestCase
+@interface KiteTests : XCTestCase
 
 @end
 
-@implementation kiteTests
+@implementation KiteTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
