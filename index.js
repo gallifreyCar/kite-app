92c6be9687bb492a9a5c9c84dc131d5418ae7e4d
RainSlide <RainSlide@noreply.codeberg.org>
2021-12-15 18:49:49 +0800

    添加主页
    顶栏实现方法也太多了，慢点写
    目前没有返回按钮和其他导航，iOS 大概是没法返回了

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-/**
- * @format
- */
+// dependencies
+import { AppRegistry, View, Text, Button } from 'react-native';
+import { NavigationContainer }        from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { AppRegistry } from 'react-native'
-import App from './src/index'
-// import { name as appName } from './app.json'
+// screens
+import Electricity from './src/index';
 
-AppRegistry.registerComponent('kite', () => App)
+const Home = ({ navigation: { navigate } }) => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Button title="电费" onPress={() => navigate('Electricity')} />
+  </View>
+);
+
+/*
+const Toolbar = () => (
+  <View>
+    <Button title="☰" onPress={() => {}} />
+    <Text>Title</Text>
+  </View>
+)
+*/
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+const Kite = () => (
+  <NavigationContainer>
+    <Navigator>
+      <Screen name="Home"        component={Home} />
+      <Screen name="Electricity" component={Electricity} />
+    </Navigator>
+  </NavigationContainer>
+);
+
+AppRegistry.registerComponent('Kite', () => Kite);
